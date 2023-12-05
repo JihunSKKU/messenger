e63b1614e29507a208c5028d03927564c5e84925
Jihun <kjh31021@g.skku.edu>
2023-12-05 23:37:11 +0900

    Fix error message

--- schemas.py
+++ schemas.py
@@ -16,7 +16,7 @@ class ChatRequest(ChatRequestBase):
     id: Optional[int]
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class ConnectionManager:
     def __init__(self):
